de5dcabf9f06016232a87a5279eed249cd902d2f
Robert James Patterson <guajiropa@hotmail.com>
2022-07-31 14:18:06 -0400

    rough outline of code in main.py

--- main.py
+++ main.py
@@ -5,3 +5,12 @@
 # SYNOPSIS  : Build a Python program that runs AutoHotkey and Powershell to automate tasks for the end-user
 #               from a simple GUI interface. 
 # 
+
+import sys
+
+
+if __name__ == "__main__":
+    app = Application(sys.argv)
+    winHero = Win10HeroForm()
+    winHero.show()
+    sys.exit(app.exec_())
